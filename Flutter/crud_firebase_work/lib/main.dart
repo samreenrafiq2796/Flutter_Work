e33bd3bad6d3f95f9aba09384ec4141f1ebfe925
samreenrafiq2796 <samreenrafiq2701@outlook.com>
2024-10-16 16:55:05 +0500

    Add and delete Work

--- Flutter/crud_firebase_work/lib/main.dart
+++ Flutter/crud_firebase_work/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:crud_firebase_work/AddData.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -43,6 +44,16 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   FirebaseFirestore db = FirebaseFirestore.instance;
 
+  void delete_record(String i){
+      try {
+        db.collection("Person").doc(i).delete();
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Record Deleted")));
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("$e")));
+      }
+  }
+
+
   @override
   Widget build(BuildContext context) {
    
@@ -60,15 +71,32 @@ class _MyHomePageState extends State<MyHomePage> {
             return ListView.builder(
               itemCount: data.length,
               itemBuilder: (context,i){    
+                if(snapshot.data!.docs.isEmpty || !snapshot.hasData){
+                  return Center(child: Text("No Data Found"),);
+                }
+                if(snapshot.connectionState == ConnectionState.waiting){
+                  return  Center(child: CircularProgressIndicator(),);
+                }
                 var per = data[i].data() as Map<String,dynamic>;
+                String person_id = data[i].id;
                 return ListTile(
                   leading: Icon(Icons.person),
                   title: Text(per["Name"]?? "no name"),
                   subtitle:  Text(per["Email"] ?? "no Email"),
+                  trailing: IconButton(icon: Icon(Icons.delete,color: Colors.red,),
+                  onPressed:(){
+                    delete_record(person_id);
+                  },),
                 );
               }
               );
-         })
+         }),
+         floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (a)=>Add()));
+          },
+          child: Icon(Icons.add),),
+
       
       // This trailing comma makes auto-formatting nicer for build methods.
     );
